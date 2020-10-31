c79bd65f47ef9eef6a2d121c788dd5382bd64a1b
sandeep.i.32 <sandeep.i.learnweel@gmail.com>
2020-10-31 18:19:03 +0530

    Updates

---
+++ precache-manifest.3b2ca7dc63e8e40e558df4bd1119e7f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67923a392837eba8a021df8a87280d6",
+    "url": "/fitbit_React/index.html"
+  },
+  {
+    "revision": "9fc0d2d1c7bc56561633",
+    "url": "/fitbit_React/static/css/main.80e9cc61.chunk.css"
+  },
+  {
+    "revision": "0ee04923f128dc5d608f",
+    "url": "/fitbit_React/static/js/2.622186f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fitbit_React/static/js/2.622186f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc0d2d1c7bc56561633",
+    "url": "/fitbit_React/static/js/main.c7800d65.chunk.js"
+  },
+  {
+    "revision": "d44f773ec23eef6427b2",
+    "url": "/fitbit_React/static/js/runtime-main.82ac19cc.js"
+  }
+]);
